37195593ed789bfc1db4f9d2b53a968503dce824
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-08-15 15:11:19 -0700

    feat(admin): add Navbar and Header components

---
+++ src/app/admin/components/Navbar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { AppShell, NavLink, ScrollArea } from '@mantine/core';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { navItems } from '../config/nav';
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <AppShell.Navbar>
+            <AppShell.Section grow my="md" component={ScrollArea} px="md">
+                {navItems.map(({ label, link }) => (
+                    <NavLink
+                        href={link}
+                        key={link}
+                        component={Link}
+                        label={label}
+                        active={link === pathname}
+                    />
+                ))}
+            </AppShell.Section>
+        </AppShell.Navbar>
+    );
+}

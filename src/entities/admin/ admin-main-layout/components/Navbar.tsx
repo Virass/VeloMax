ec066b3f9f5a034ea348a0c7dab65f4f5713ce61
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-08-18 14:19:00 -0700

    fix: reusing data from global URLs object + fix url naming

--- src/entities/admin/ admin-main-layout/components/Navbar.tsx
+++ src/entities/admin/ admin-main-layout/components/Navbar.tsx
@@ -4,23 +4,28 @@ import { AppShell, NavLink, ScrollArea } from '@mantine/core';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { navItems } from '../config/nav';
+import { URLs } from '@/shared/constants/urls';
 
 export default function Navbar() {
     const pathname = usePathname();
+    const navItems = Object.entries(URLs.admin).map(([label, link]) => ({
+        label,
+        link,
+    }));
 
     return (
         <AppShell.Navbar>
             <AppShell.Section grow my="md" component={ScrollArea} px="md">
                 {navItems.map(({ label, link }) => {
                     const isActive = link === pathname;
+                    const formattedLabel = `${label.charAt(0).toUpperCase()}${label.slice(1)}`;
 
                     return (
                         <NavLink
                             href={link}
                             key={link}
                             component={Link}
-                            label={label}
+                            label={formattedLabel}
                             active={isActive}
                             disabled={isActive}
                         />

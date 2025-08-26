f59fa448149fc6f3106bb786e9536a164d43d100
Maryna Mazur <107570670+maryna-mazur@users.noreply.github.com>
2025-08-26 13:36:51 +0300

    Feature/website home page (#9)
    * chore: install prettier
    
    * feat: add WebsiteLayout with Header and start header implementation
    
    * feat: refactor layout components and add phone number formatting utility
    
    * fix: linter problems
    
    ---------
    
    Co-authored-by: Virass <virass.m@gmail.com>

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Stack } from '@mantine/core';
+import { Roboto } from 'next/font/google';
+
+import { Footer } from '@/entities/website/Footer';
+import { Header } from '@/entities/website/Header';
+
+const roboto = Roboto({
+    variable: '--font-roboto',
+    subsets: ['latin', 'cyrillic'],
+});
+
+export default function WebsiteLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className={roboto.className}>
+            <Stack
+                mih="100dvh"
+                px={{ base: 16, sm: 20, md: 24 }}
+                py={{ base: 8, sm: 12, md: 16 }}
+            >
+                <Header />
+                <main style={{ flex: 1 }}>{children}</main>
+                <Footer />
+            </Stack>
+        </div>
+    );
+}

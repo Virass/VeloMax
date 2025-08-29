ff400a2563ce8fc35fd8e53ef08c1664c6b39bf1
Virass <virass.m@gmail.com>
2025-08-30 01:08:27 +0200

    feat: restructure admin layout and components, implement auth guard

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,6 @@
+import { URLs } from "@/shared/constants/urls";
+import { redirect, RedirectType } from "next/navigation";
+
+export default function AuthPage() {
+    redirect(URLs.auth.signIn, RedirectType.push);
+}

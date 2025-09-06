4abf302e10ee4f38c7300bba5a58fe01054ad0af
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-09-06 14:29:55 -0700

    chore: components & styles

---
+++ src/shared/components/FooterNavigationSection.tsx
@@ -0,0 +1,19 @@
+import { Text } from '@mantine/core';
+import Link from 'next/link';
+
+interface Props {
+    label: string;
+    link: string;
+}
+
+import styles from '@/shared/styles/website-footer.module.css';
+
+export default function FooterNavigationSection({ label, link }: Props) {
+    return (
+        <Link key={label} href={link} className={styles.footerNavigationItem}>
+            <Text tt="capitalize" c="gray.0">
+                {label}
+            </Text>
+        </Link>
+    );
+}

c469f81de1a898a725dfc32a1e831be5dca2a9f1
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-09-12 14:30:37 -0700

    fix: categories sidebar

--- src/entities/website/Categories/SideBar/SideBar.tsx
+++ src/entities/website/Categories/SideBar/SideBar.tsx
@@ -1,48 +1,28 @@
 import { Stack, Text } from '@mantine/core';
 
-import CheckboxField from '@/shared/components/CheckboxInput';
-import RangeInput from '@/shared/components/RangeInput';
+import { inter } from '@/app/layout';
 
+import { FilterItem } from '../FilterItem';
 import { getFilters } from './services/sidebar.service';
 
 export default async function SideBar() {
     const filters = await getFilters();
 
     return (
-        <Stack visibleFrom="lg" w="500px">
-            <Text size="lg">Категорії</Text>
+        <Stack
+            visibleFrom="lg"
+            bg="gray.1"
+            bdrs="48px"
+            p="48px 12px 48px 100px"
+            gap="32px"
+        >
+            <Text size="34px" className={inter.className}>
+                Категорії
+            </Text>
 
-            <Stack>
-                {filters.map(({ name, type, options }) => (
-                    <Stack key={name}>
-                        <Text fw="bold">{name}</Text>
-
-                        {type === 'range' && (
-                            <RangeInput
-                                marks={options.map((option) => ({
-                                    value:
-                                        (((option as number) - 0) /
-                                            ((options[
-                                                options.length - 1
-                                            ] as number) -
-                                                0)) *
-                                        100,
-                                    label: `${option} UAH`,
-                                }))}
-                            />
-                        )}
-
-                        {type === 'checkbox' && (
-                            <Stack gap="16px">
-                                {options.map((option) => (
-                                    <CheckboxField
-                                        key={option}
-                                        label={option}
-                                    />
-                                ))}
-                            </Stack>
-                        )}
-                    </Stack>
+            <Stack gap="48px">
+                {filters.map((filter) => (
+                    <FilterItem key={filter.name} filter={filter} />
                 ))}
             </Stack>
         </Stack>

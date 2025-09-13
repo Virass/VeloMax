f3c6e75c92d8d1a628c817261fb9327218bbf1f1
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-09-13 15:22:35 -0700

    feat: implement TopBar component

--- src/entities/website/Categories/TopBar/TopBar.tsx
+++ src/entities/website/Categories/TopBar/TopBar.tsx
@@ -1,10 +1,35 @@
-'use client';
+import { Box, Group } from '@mantine/core';
 
-import { rem } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
+import Breadcrumbs from '@/shared/components/Breadcrumbs';
+import { Button } from '@/shared/components/Button';
+import FilterIcon from '@/shared/ui/icons/FilterIcon';
+import LeftArrowIcon from '@/shared/ui/icons/LeftArrowIcon';
+
+const buttons = [
+    {
+        label: 'Назад',
+        icon: <LeftArrowIcon color="gray.9" height={10} width={16} />,
+    },
+    {
+        label: 'Фільтрувати',
+        icon: <FilterIcon color="gray.9" height={16} width={16} />,
+    },
+];
 
 export default function TopBar() {
-    const isMobile = useMediaQuery(`(max-width: ${rem(750)})`);
+    return (
+        <>
+            <Group hiddenFrom="sm" justify="space-between">
+                {buttons.map(({ label, icon }) => (
+                    <Button key={label} leftIcon={icon} variant="invisible">
+                        {label}
+                    </Button>
+                ))}
+            </Group>
 
-    return isMobile ? <div>Mobile Action Bar</div> : <div>Breadcrumbs</div>;
+            <Box visibleFrom="sm" bg="gray.1" p="48px 64px" bdrs="48px">
+                <Breadcrumbs />
+            </Box>
+        </>
+    );
 }
---
+++ src/shared/components/Breadcrumbs.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Group, Text } from '@mantine/core';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { websiteSections } from '../constants/urls';
+
+export default function Breadcrumbs() {
+    const pathname = usePathname();
+
+    const splittedPathname = pathname.split('/').map((path) => `/${path}`);
+
+    const breadcrumbSections = splittedPathname
+        .map((path) => websiteSections.find((section) => section.link === path))
+        // Bypass ts -> remove and fix later
+        .filter((section): section is NonNullable<typeof section> => !!section);
+
+    return (
+        <Group gap="0.25rem">
+            {breadcrumbSections.map((section, index) => (
+                <Text
+                    key={section.link}
+                    component={Link}
+                    href={section.link}
+                    variant="link"
+                    tt="capitalize"
+                >
+                    {section.label}
+                    {index < breadcrumbSections.length - 1 && (
+                        <Text span color="gray.6">
+                            {' '}
+                            /
+                        </Text>
+                    )}
+                </Text>
+            ))}
+        </Group>
+    );
+}

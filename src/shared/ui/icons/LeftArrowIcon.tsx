d4cef541ce0c62df95debcb5c7c8863e39c09b13
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-09-27 15:16:35 -0700

    fix: center LeftArrowIcon

--- src/shared/ui/icons/LeftArrowIcon.tsx
+++ src/shared/ui/icons/LeftArrowIcon.tsx
@@ -4,8 +4,8 @@ import Icon from '@/shared/components/Icon';
 import type { IconProps } from '@/shared/types/icon';
 
 export default function LeftArrowIcon({
-    width,
-    height,
+    width = 24,
+    height = 24,
     color,
     mode,
 }: IconProps) {
@@ -16,7 +16,9 @@ export default function LeftArrowIcon({
             color={color}
             mode={mode}
             path={
-                <path d="M5.707.293a1 1 0 0 1 0 1.414L3.414 4H15a1 1 0 1 1 0 2H3.414l2.293 2.293a1 1 0 1 1-1.414 1.414l-4-4a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0Z" />
+                <g transform="translate(4 7)">
+                    <path d="M5.707.293a1 1 0 0 1 0 1.414L3.414 4H15a1 1 0 1 1 0 2H3.414l2.293 2.293a1 1 0 1 1-1.414 1.414l-4-4a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0Z" />
+                </g>
             }
         />
     );

eb3a717e7e391d44a4a84f49656838a606731fab
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-10-04 11:43:33 -0700

    add responsiveness to the product page

--- src/features/website/Product/Price/Price.tsx
+++ src/features/website/Product/Price/Price.tsx
@@ -5,19 +5,25 @@ interface Props {
     discountPrice?: number;
 }
 
+import styles from '../styles/product.module.scss';
+
 export default function Price({ price, discountPrice }: Props) {
     return (
         <Group gap="3px">
             <Text
                 c="gray.6"
                 fw="700"
-                fz="20px"
                 td={discountPrice ? 'line-through' : 'none'}
+                className={styles.productContentContainer__title}
             >
                 {price} UAH
             </Text>
             {discountPrice && (
-                <Text c="red.9" fw="700" fz="20px">
+                <Text
+                    c="red.9"
+                    fw="700"
+                    className={styles.productContentContainer__title}
+                >
                     {discountPrice} UAH
                 </Text>
             )}

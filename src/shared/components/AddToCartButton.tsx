438720a5d63206e6d26d38728321275f4b536960
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-11-25 22:09:06 -0800

    fix: create a ClientOnly component to handle the hydration error

--- src/shared/components/AddToCartButton.tsx
+++ src/shared/components/AddToCartButton.tsx
@@ -5,6 +5,7 @@ import { Text, type ButtonProps } from '@mantine/core';
 import { type CartItem, useCartStore } from '@/core/store/useShoppingCartStore';
 
 import { Button } from './Button';
+import { ClientOnly } from './ClientOnly';
 import { useStore } from '../hooks/useStore';
 
 interface Props extends ButtonProps {
@@ -17,11 +18,11 @@ export default function AddToCartButton({
     cartItem,
     ...rest
 }: Props) {
-    const addItem = useStore(useCartStore, (state) => state.addItem);
-    const removeItem = useStore(useCartStore, (state) => state.removeItem);
-    const shoppingCartItems = useStore(useCartStore, (state) => state.items);
-
-    // console.log(cartItem);
+    const {
+        addItem,
+        removeItem,
+        items: shoppingCartItems,
+    } = useStore(useCartStore, (state) => state);
 
     const isInCart = cartItem
         ? shoppingCartItems.some((item) => item.id === cartItem.id)
@@ -40,19 +41,21 @@ export default function AddToCartButton({
     };
 
     return (
-        <Button
-            bdrs="32px"
-            bg={`gray.${availability ? '9' : '2'}`}
-            disabled={!availability}
-            c={availability ? 'white' : 'gray.5'}
-            w="100%"
-            size="lg"
-            onClick={toggle}
-            {...rest}
-        >
-            <Text size="18px">
-                {isInCart ? 'Видалити з кошика' : 'Додати до кошика'}
-            </Text>
-        </Button>
+        <ClientOnly>
+            <Button
+                bdrs="32px"
+                bg={`gray.${availability ? '9' : '2'}`}
+                disabled={!availability}
+                c={availability ? 'white' : 'gray.5'}
+                w="100%"
+                size="lg"
+                onClick={toggle}
+                {...rest}
+            >
+                <Text size="18px">
+                    {isInCart ? 'Видалити з кошика' : 'Додати до кошика'}
+                </Text>
+            </Button>
+        </ClientOnly>
     );
 }
